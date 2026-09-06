95580d9d84708b54709a1072bd13a414edc5bc18
Marcelo Detlefsen <marcelodetlefsen1a@gmail.com>
2026-09-06 11:55:24 -0600

    Se corrigen problemas de paths del monorepo que no permitían correr lint ni test ni build.

--- widget/eslint.config.ts
+++ widget/eslint.config.ts
@@ -1,8 +1,8 @@
-import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue from 'eslint-plugin-vue'
-import pluginVitest from '@vitest/eslint-plugin'
-import pluginOxlint from 'eslint-plugin-oxlint'
+import { globalIgnores } from '../node_modules/.bun/eslint@10.8.1+1a1acd4c2fa5b1a4/node_modules/eslint/lib/config-api.js'
+import { defineConfigWithVueTs, vueTsConfigs } from '../node_modules/.bun/@vue+eslint-config-typescript@14.9.0+4739fbbd95b8d73c/node_modules/@vue/eslint-config-typescript/dist/index.mjs'
+import pluginVue from '../node_modules/.bun/eslint-plugin-vue@10.9.2+d1f2631c5cd8e498/node_modules/eslint-plugin-vue/dist/index.js'
+import pluginVitest from '../node_modules/.bun/@vitest+eslint-plugin@1.6.27+001564ad9ebcb0c0/node_modules/@vitest/eslint-plugin/dist/index.mjs'
+import pluginOxlint from '../node_modules/.bun/eslint-plugin-oxlint@1.73.0+16f7eb975a1083a1/node_modules/eslint-plugin-oxlint/dist/index.mjs'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
